8c23cf2599b866ef504473ab183478a10796f81e
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-10 12:02:06 +0000

    container config done

---
+++ 01_basics/03_conversionOperaton.js
@@ -0,0 +1,16 @@
+// let score = "30" 
+
+// console.log(typeof (score));
+// // string to number
+// let stringInNumber = Number(score)
+// console.log(stringInNumber);
+// console.log(typeof (stringInNumber));
+
+let mixString = "234kishan"
+let covertStoN = Number(mixString)
+console.log(typeof(covertStoN));
+console.log(covertStoN);
+
+
+
+
