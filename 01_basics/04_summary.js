93cc608f205e01159d168ad81ba3b446b3da8146
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-11 09:46:45 +0000

    container config done

---
+++ 01_basics/04_summary.js
@@ -0,0 +1,18 @@
+// data types
+
+// primitive = string, number, boolean, null, undefined, symbol, BigInt
+
+// non primitive(Reference) = Array, Object, Function
+
+const marvel = ["ironman", "thor", "hulk"]; //=> array
+
+const myObj = {
+    name:"kishan",
+    age:20,
+    gender:"male"
+}                      //=> Object
+
+
+const myFun1 = function(){
+    console.log("kishan")
+}                               //=> Function
