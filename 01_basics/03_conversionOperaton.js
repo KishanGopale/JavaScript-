f51f546bf93e7722e06cd0a2495c857b3496ba25
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-11 09:07:55 +0000

    container config done

--- 01_basics/03_conversionOperaton.js
+++ 01_basics/03_conversionOperaton.js
@@ -6,11 +6,42 @@
 // console.log(stringInNumber);
 // console.log(typeof (stringInNumber));
 
-let mixString = "234kishan"
-let covertStoN = Number(mixString)
-console.log(typeof(covertStoN));
-console.log(covertStoN);
+// let mixString = "234kishan"
+// let covertStoN = Number(mixString)
+// console.log(typeof(covertStoN));
+// console.log(covertStoN);
 
 
 
+// ********************OPERATION**********************
 
+
+let value = 5
+
+let negativeValue = -value
+
+console.log(negativeValue);
+
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+
+// add two string 
+
+// let str1 = "hello"
+// let str2 = "Kishan"
+
+// let str3 = str1+str2
+
+// console.log(str3)
+
+
+console.log("1"+2);
+console.log(1+"3");
+console.log("4"+9+3);
+console.log(8+3+"6");
