13fc52c5190cbd8e1a3050c4fad42a586ad733b0
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-12 09:59:34 +0000

    container config done

--- 01_basics/04_summary.js
+++ 01_basics/04_summary.js
@@ -4,15 +4,55 @@
 
 // non primitive(Reference) = Array, Object, Function
 
-const marvel = ["ironman", "thor", "hulk"]; //=> array
+// const marvel = ["ironman", "thor", "hulk"]; //=> array
 
-const myObj = {
-    name:"kishan",
-    age:20,
-    gender:"male"
-}                      //=> Object
+// const myObj = {
+//     name:"kishan",
+//     age:20,
+//     gender:"male"
+// }                      //=> Object
 
 
-const myFun1 = function(){
-    console.log("kishan")
-}                               //=> Function
+// const myFun1 = function(){
+//     console.log("kishan")
+// }                               //=> Function
+
+
+
+
+// ******************** MEMORY **********************
+
+// 1. Stack is used for primitive data types
+// 2. Heap is used for non-primitive data types
+
+
+let myName = "kishan"
+
+let mySecondName = myName
+
+mySecondName = "Vyankatesh"
+
+console.log(myName);
+console.log(mySecondName);
+
+
+/////// output willl be differrent 
+// kishan
+// vyankatesh
+
+
+const userOne = {
+    email : "ABC@gmail",
+    name : "ABC"
+}
+
+let userTwo = userOne 
+
+userTwo.email="PQR@gmail"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+////// output will be same 
+//PQR@gmail
+//PQR@gmail
