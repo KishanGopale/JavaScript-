52170fb5c1ee0ab976bd0aa9c0b7821ab3897dc9
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-13 14:48:33 +0000

    container config done

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,26 @@
+// const firstnum = 345
+
+// const secnum = new Number(10063428)
+
+// console.log(firstnum);
+// console.log(secnum);
+
+// console.log(secnum.toFixed(1));
+
+// console.log(secnum.toLocaleString('en-IN'))
+
+
+
+
+// ***************** MATH ******************
+
+// console.log(Math.random())
+
+// console.log((Math.abs(0.254) * 10) + 1); 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
