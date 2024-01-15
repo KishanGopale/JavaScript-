1b767e47fbf46171db854002ab5a38b5388423aa
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-15 17:35:55 +0000

    container config done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,29 @@
+// array
+
+const myArr = [2,3,5,6,3,8,5,9]
+
+// console.log(myArr[2]);
+
+// array method
+
+//  myArr.push(23)
+//  console.log(myArr)
+
+// myArr.pop();
+// console.log(myArr)
+
+// myArr.unshift(22)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// const myNewArray = myArr.join()
+
+// console.log(myArr);
+// console.log(myNewArray);
+
+// const m1 = myArr.slice(2,4)
+const m2 = myArr.splice(2,4)
+
+console.log(m2);
