f9a36aaa7219936c9ad54e7d581ec17f1970d998
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-01-16 16:49:53 +0000

    container config done

---
+++ 02_basics/01_array2.js
@@ -0,0 +1,21 @@
+const marvel_heros = ['thor', 'ironman', 'spiderman']
+
+const dc_heros = [' superman', 'batman', 'flash']
+
+// mandd = marvel_heros.concat(dc_heros)
+
+// console.log(mandd);
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+// const arr = [...marvel_heros, ...dc_heros];
+// console.log(arr)
+
+const arr1 = [12,35,[5,4,[36,5],7,2],72,3]
+
+const simplify_arry = arr1.flat(Infinity)
+
+console.log(simplify_arry);
